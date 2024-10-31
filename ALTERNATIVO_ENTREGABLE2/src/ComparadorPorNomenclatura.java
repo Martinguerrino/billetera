7e4d73e46ad24a562a5b939fd0dab21e649c3004
Martin Guerrino <98595801+Martinguerrino@users.noreply.github.com>
2024-10-31 18:15:53 -0300

    Delete ALTERNATIVO_ENTREGABLE2 directory

--- ALTERNATIVO_ENTREGABLE2/src/ComparadorPorNomenclatura.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class ComparadorPorNomenclatura implements Comparator<Moneda> {
-    @Override
-    public int compare(Moneda m1, Moneda m2) {
-        return m1.getNomenclatura().compareTo(m2.getNomenclatura());
-    }
-}
--- ALTERNATIVO_ENTREGABLE2/src/ComparadorPorStockDescendente.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-// Comparador para ordenar por stock descendente
-public class ComparadorPorStockDescendente implements Comparator<Moneda> {
-    
-    @Override
-    public int compare(Moneda m1, Moneda m2) {
-        return Float.compare(m2.getStock(), m1.getStock());
-    }
-}
