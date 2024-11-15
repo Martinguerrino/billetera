e624650aeadafe80c327e9619f3fc8121e2ac173
Martinguerrino <martonguerrino@gmail.com>
2024-11-15 16:21:44 -0300

    Add methods for transaction management in ServicioTransacciones class: crearTransaccion, listarTransacciones, and imprimirTransacciones

--- Entregable3/src/billetera/Modelo/Servicios/ServicioTransacciones.java
+++ Entregable3/src/billetera/Modelo/Servicios/ServicioTransacciones.java
@@ -1,6 +1,52 @@
 package billetera.Modelo.Servicios;
 
+import billetera.Modelo.DAO.Transaccion;
+import billetera.Modelo.DAO.TransaccionDAOjdbc;
+import java.sql.SQLException;
+import java.util.List;
+
 public class ServicioTransacciones 
 {
+    public void crearTransaccion(Transaccion transaccion) throws SQLException
+    {
+        TransaccionDAOjdbc transaccionDAO = new TransaccionDAOjdbc();
+        try{
+            transaccionDAO.crearTransaccion(transaccion);
+            }
+        catch (SQLException e){
+            System.out.println("Error al crear la transaccion");
+    }
+    }
+    public List<Transaccion> listarTransacciones() throws SQLException
+    {
+        TransaccionDAOjdbc transaccionDAO = new TransaccionDAOjdbc();
+        try 
+        {
+            List<Transaccion> transacciones = TransaccionDAOjdbc.listarTransacciones();
+            return transacciones;
+        } 
+        catch (SQLException e) 
+        {
+            System.out.println("Error al listar las transacciones");
+            return null;
+        }
+        
+        
+    }
+    public void imprimirTransacciones()
+    {
+        try 
+        {
+            List<Transaccion> transacciones = listarTransacciones();
+            for (Transaccion transaccion : transacciones) 
+            {
+                System.out.println(transaccion);
+            }
+        } 
+        catch (SQLException e) 
+        {
+            System.out.println("Error al imprimir las transacciones");
+        }
+    }
     
 }
