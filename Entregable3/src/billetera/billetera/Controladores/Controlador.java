cccce262996fcf4db0695927bd461e762be91fff
Martinguerrino <martonguerrino@gmail.com>
2024-12-06 19:00:00 -0300

    Add wallet management and session control methods to Controlador class

--- Entregable3/src/billetera/billetera/Controladores/Controlador.java
+++ Entregable3/src/billetera/billetera/Controladores/Controlador.java
@@ -20,6 +20,38 @@ public class Controlador
         this.controladorCuenta();
         
     }
+    public void controladorBilletera()
+    {
+        //para mostrar los activos deberia la vista acceder al servicio activos
+        Vista vista = new Vista();
+        vista.mostrarActivos();
+        vista.mostrarBalance();
+        while(!botonDeirse)
+        {
+        }
+        this.controladorCuenta();
+    }
+    public void controladorCerrarSesion()
+    {
+        Vista vista = new Vista();
+        vista.mostrarCerrarSesion();
+        //aca nose si deberia mostrar el login y la otra opcion de cerrar la app 
+        // o que corte directamente la app
+    }
+    public void controladorCompra()
+    {
+        Vista vista = new Vista();
+        vista.mostrarCompra();
+        //aca deberia mostrar la lista de productos y el boton de comprar
+        //aca deberia acceder al servicioCompra
+        while(Modelo.ServicioCompra.comprar(monedaFiat,cantidad,moneda))
+        {
+            vista.mostrarErrorCompra();
+            vista.mostrarCompra();
+        }
+
+        this.controladorCuenta();
+    }
     public void controladorCuenta()
     {
         Vista vista = new Vista();
@@ -28,7 +60,7 @@ public class Controlador
         switch(opcion)
         {
             case 1:
-                //controlador de la billetera
+                //controlador de la billetera que permitira ver balance y activos
                 this.controladorBilletera();
                 break;
             case 2:
@@ -37,13 +69,10 @@ public class Controlador
                 break;
             
             case 4:
-                //controlador de la transferencia
-                this.controladorTransferencia();
+                //controlador de cotizaciones
+                this.controladorCotizaciones();
                 break;
-            case 5:
-                //controlador de la inversion
-                this.controladorInversion();
-                break;
+            
             case 6:
                 //controlador de la inversion
                 this.controladorCerrarSesion();
